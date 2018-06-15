db9e87b35b509773fa34f6eb053420f233d7bb13
George <ga.timbrell@googlemail.com>
2018-06-15 09:40:49 +0100

    first commit

---
+++ calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+       int num1 = 5;
+       int num2 = 6;
+       float result;
+
+       result = num1*num2;
+
+       System.out.println(num1 + " * " + num2 + " = " + result);
+
+        result = num1/num2;
+
+        System.out.println(num1 + " / " + num2 + " = " + result);
+
+        result = num1+num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + result);
+
+        result = num1-num2;
+
+        System.out.println(num1 + " - " + num2 + " = " + result);
+    }
+}
